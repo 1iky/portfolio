3ac8dc86ca6f4874d9f1ddf12eef37f70fbd532d
lso9@uwo.ca <lso9@uwo.ca>
2025-01-14 02:13:11 -0500

    Added new projects!

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -10,6 +10,8 @@ import wiggletale from './assets/wiggletale.png';
 import portfolio from './assets/portfolio.png';
 import bettertogether from './assets/bettertogether.png';
 import colourfulcritterquest from './assets/colourfulcritterquest.png';
+import fintelligent from './assets/fintelligent.png';
+import Alignify from './assets/Alignify.png';
 
 
 export default function Projects() {
@@ -80,6 +82,66 @@ export default function Projects() {
     
             <div className='projects-container'>
                 <div className='projects-list'>
+                    {/* Project 1 Fintelligent*/}
+                    <div className='project'>
+                        <div className='project-image'>
+                            <img src={fintelligent} alt='project-image'/>
+                        </div>
+                        <div className='project-details'>
+                            <div className='project-header'>
+                                <h2 className='project-title'>
+                                    Fintelligent
+                                </h2>
+                                <div className='project-tags'>
+                                    <span className='tag'>React</span>
+                                    <span className='tag'>Office Javascript API</span>
+                                    <span className='tag'>FastAPI</span>
+                                    <span className='tag'>Pandas</span>
+                                    <span className='tag'>OpenAI API</span>
+                                </div>
+                            </div>
+                            <p className='project-description'>
+                                ✦ Fintelligent is an AI-powered Excel add-in that integrates a natural language interface for users to ask questions, explain formulas, and generate insights using OpenAI's GPT-4, while automating financial workflows and enhancing data analysis
+                                <br></br>
+                                ✦ Built using FastAPI, OpenAI API, React, Office JavaScript API, and Pandas, ensuring smooth integration and enhanced user experience.
+                                <br></br>
+                                ✦ Streamlined data entry, formatting, and repetitive calculations to enhance workflow efficiency.
+                                
+                            </p>
+                            <div className='project-links'>
+                                <a href='https://github.com/1iky/portfolio' target='_blank' rel='noreferrer' className='project-button'>
+                                    Visit github 
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Project 2 Alignify*/}
+                    <div className='project'>
+                        <div className='project-image'>
+                            <img src={Alignify} alt='project-image'/>
+                        </div>
+                        <div className='project-details'>
+                            <div className='project-header'>
+                                <h2 className='project-title'>
+                                    Alignify
+                                </h2>
+                                <div className='project-tags'>
+                                    <span className='tag'>C++</span>
+                                    <span className='tag'>Qt</span>
+                                </div>
+                            </div>
+                            <p className='project-description'>
+                                ✦ Developed a Qt and C++ app that simplifies group scheduling by showing shared calendars and availability.
+                                <br></br>
+                                ✦ Automatically identifies the best times for meetings and highlights potential attendees based on their schedules.
+                                <br></br>
+                                ✦ Implemented Object-Oriented Programming (OOP) and the MVC design pattern for maintainable and scalable code.
+                            </p>
+                            <div className='project-links'>
+                                
+                            </div>
+                        </div>
+                    </div>
                     {/* Project 1 Wiggletale*/}
                     <div className='project'>
                         <div className='project-image'>
